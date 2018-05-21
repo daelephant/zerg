51158f856e24f64a26669785d91fd74e3fe35b50
elephant2 <2224168716@qq.com>
2018-05-21 18:33:33 +0800

    add pathinfo

---
+++ application/sample/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyx
+ * Date: 2018-05-21
+ * Time: 17:02
+ */
+
+namespace app\sample\controller;
+
+
+class Test
+{
+    public function hello()
+    {
+        return "hello,yin";
+    }
+}
