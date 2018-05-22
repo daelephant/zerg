17d42b2368b61955e4788dc051010a903fac6c4d
elephant2 <2224168716@qq.com>
2018-05-22 18:30:32 +0800

    add pathinfo3

--- application/route.php
+++ application/route.php
@@ -9,13 +9,9 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-
-];
+use think\Route;
+//Route::rule('hello','sample/Test/hello');
+//Route::rule('hello','sample/Test/hello','GET|POST',['https'=>false]);
+//Route::get('hello/:id','sample/Test/hello');
+//Route::get('hello/:id','sample/Test/hello');
+Route::post('hello/:id','sample/Test/hello');
--- application/sample/controller/Test.php
+++ application/sample/controller/Test.php
@@ -8,11 +8,17 @@
 
 namespace app\sample\controller;
 
-
+use think\Request;
 class Test
 {
     public function hello()
     {
-        return "hello,yin";
+        $all = input('param.');
+        var_dump($all);
+        //$id = Request::instance()->param('id');
+        //$name = Request::instance()->param('name');
+        //$age = Request::instance()->param('age');
+        //echo $id.'------'.$name.'+++++'.$age;
+        //return "hello,yin";
     }
 }
