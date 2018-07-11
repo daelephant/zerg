8440bfc12ebc8829c861e7ab81b2082f4d91adc8
elephant2 <2224168716@qq.com>
2018-07-11 18:10:45 +0800

    repair the bug：null...

---
+++ application/api/controller/v1/Order.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyx
+ * Date: 2018-07-11
+ * Time: 17:40
+ */
+
+namespace app\api\controller\v1;
+
+
+use think\Controller;
+
+class Order extends Controller
+{
+
+}
