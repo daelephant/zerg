7cc4de5ac026daabd0bd7c6e88b699e6ef76a542
yin <2224168716@qq.com>
2018-07-16 18:44:46 +0800

    Pay API sign

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -11,6 +11,7 @@ namespace app\api\controller\v1;
 
 use app\api\controller\BaseController;
 use app\api\validate\IDMustBePostiveInt;
+use app\api\service\Pay as PayService;
 
 class Pay extends BaseController
 {
@@ -21,5 +22,7 @@ class Pay extends BaseController
     public function getPreOrder($id='')
     {
         (new IDMustBePostiveInt())->goCheck();
+        $pay = new PayService($id);
+        $pay->pay();
     }
 }
--- application/api/service/Pay.php
+++ application/api/service/Pay.php
@@ -14,7 +14,10 @@ use app\lib\enum\OrderStatusEnum;
 use app\lib\exception\OrderException;
 use app\lib\exception\TokenException;
 use think\Exception;
+use think\Loader;
 
+//extend/WxPay/WxPay.Api.php
+Loader::import('WxPay.WxPay',EXTEND_PATH,'.Api.php');
 class Pay
 {
     private $orderNo;
@@ -47,7 +50,50 @@ class Pay
         {
             return $status;
         }
+        return $this->makeWxPreOrder($status['orderPrice']);
     }
+
+    private function makeWxPreOrder($totalPrice)
+    {
+        //openid
+        $openid = Token::getCurrentTokenVar('openid');
+        if(!$openid)
+        {
+            throw new TokenException();
+        }
+        $wxOrderData = new \WxPayUnifiedOrder();
+        $wxOrderData->SetOut_trade_no($this->orderNo);
+        $wxOrderData->SetTrade_type('JSAPI');
+        $wxOrderData->SetTotal_fee($totalPrice * 100);//单位是分，所以要转换成元
+        $wxOrderData->SetBody('零食商贩');
+        $wxOrderData->SetOpenid($openid);
+        $wxOrderData->SetNotify_url(config('secure.pay_back_url'));//微信异步通知支付结果
+
+        return $this->getPaySignature($wxOrderData);
+    }
+
+    //向微信请求订单号并生成签名
+    private function getPaySignature($wxOrderData)
+    {
+        $wxOrder = \WxPayApi::unifiedOrder($wxOrderData);
+        // 失败时不会返回result_code
+        if($wxOrder['return_code'] != 'SUCCESS' || $wxOrder['result_code'] !='SUCCESS'){
+            Log::record($wxOrder,'error');
+            Log::record('获取预支付订单失败','error');
+            //            throw new Exception('获取预支付订单失败');
+        }
+        //prepay_id
+        $this->recordPreOrder($wxOrder);
+        $signature = $this->sign($wxOrder);
+        return $signature;
+    }
+
+    private function recordPreOrder($wxOrder){
+        // 必须是update，每次用户取消支付后再次对同一订单支付，prepay_id是不同的
+        OrderModel::where('id', '=', $this->orderID)
+            ->update(['prepay_id' => $wxOrder['prepay_id']]);
+    }
+
 
     private function checkOrderValid()
     {
